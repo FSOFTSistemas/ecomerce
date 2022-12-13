5b3015a23a05b9b152ac8ce50e3bb770a53ef5ee
Breno Macedo Barkokebas <49546135+brenomacedodm@users.noreply.github.com>
2022-12-13 17:57:13 -0300

    alterando user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
+            $table->string('telefone');
+            $table->string('cpf')->nullable();
+            $table->string('rg')->nullable();
+            $table->foreign('endereco_id')->references('id')->on('enderecos')->nullable();;
+            $table->enum("tipo",["cliente","admin"]);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
