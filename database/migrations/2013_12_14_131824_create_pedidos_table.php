811abbe294da87948100fe96274ee0ca727d1345
Breno Macedo Barkokebas <49546135+brenomacedodm@users.noreply.github.com>
2022-12-14 17:35:27 -0300

    criando pedido

---
+++ database/migrations/2013_12_14_131824_create_pedidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->date('data');
+            $table->enum('stastus',['pendente','finalizado']);
+            $table->decimal('total');
+            $table->decimal('subtotal');
+            $table->decimal('desconto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
