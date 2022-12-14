d34b21f75d91cf12a394ef683292ddcf41bca81c
Breno Macedo Barkokebas <49546135+brenomacedodm@users.noreply.github.com>
2022-12-14 17:39:38 -0300

    criando item pedido

---
+++ database/migrations/2022_12_14_180207_create_item_pedidos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained('pedidos');
+            $table->foreignId('produto_id')->constrained('produtos');
+            $table->integer('quatidade');
+            $table->decimal('total');
+            $table->decimal('subtotal');
+            $table->decimal('desconto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_pedidos');
+    }
+};
