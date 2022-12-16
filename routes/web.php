330f8b7abb6e41344180a17734c1eaa99a2732c8
Breno Macedo Barkokebas <49546135+brenomacedodm@users.noreply.github.com>
2022-12-16 17:35:35 -0300

    alterando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
@@ -24,6 +26,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ProdutoController::class, 'welcome'])->name('home');
 Route::get('/registrar', [UsuarioController::class, 'create'])->name('registrar');
 Route::post('/registrar', [UsuarioController::class, 'store'])->name('user.store');
+Route::get('/carrinho', [PedidoController::class, 'index'])->name('carrinho');
+Route::post('/entrar', [LoginController::class, 'entrar'])->name('login.entrar');
+
 
 
 Route::get('/dashboard', function () {
