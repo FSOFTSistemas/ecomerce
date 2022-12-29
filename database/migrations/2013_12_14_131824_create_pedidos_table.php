d661f2385676228f792ab8c7c03ee9193fda232a
Breno Macedo Barkokebas <49546135+brenomacedodm@users.noreply.github.com>
2022-12-29 10:24:01 -0300

    corrigindo nome do atributo status

--- database/migrations/2013_12_14_131824_create_pedidos_table.php
+++ database/migrations/2013_12_14_131824_create_pedidos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('pedidos', function (Blueprint $table) {
             $table->id();
             $table->date('data')->nullable();
-            $table->enum('stastus',['pendente','finalizado'])->nullable();
+            $table->enum('status',['pendente','aberto','finalizado'])->nullable();
             $table->foreignId('user_id')->constrained('users')->nullable();
             $table->decimal('total')->nullable();
             $table->decimal('subtotal')->nullable();
