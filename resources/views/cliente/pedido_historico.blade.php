3d19f0cd2a42c2c7b197504f7a4158fc6a784f41
Breno Macedo Barkokebas <49546135+brenomacedodm@users.noreply.github.com>
2023-01-03 16:40:54 -0300

    cliente visualizar historico de pedidos

--- resources/views/cliente/pedido_historico.blade.php
+++ resources/views/cliente/pedido_historico.blade.php
@@ -5,11 +5,11 @@
 @section('conteudo')
     <div class="span9">
         <ul class="breadcrumb">
-            <li><a href="{{ route('home')}}">Home</a> <span class="divider">/</span></li>
-            <li class="active"> Carrinho</li>
+            <li><a href="{{ route('home')}}">Home</a> <span class="divider">/</span> Carrinho <span class="divider">/</span></li>
+            <li class="active"> Histórico</li>
         </ul>
-        <h3> Carrinho<a href="{{ route('pedido.historico')}}" class="btn btn-large pull-right">
-             Histórico de pedidos </a>
+        <h3> Histórico de pedidos <a href="#" class="btn btn-large pull-right" onclick="location.href = document.referrer;">
+            Voltar</a>
         </h3>
         <hr class="soft" />
         {{-- <table class="table table-bordered">
@@ -51,43 +51,19 @@
         <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th>Produto</th>
-                    <th>Descricao</th>
-                    <th>Quantidade</th>
-                    <th>Preço</th>
-                    <th>Desconto</th>
-                    <th colspan="2">Total</th>
+                    <th>Nº do pedido</th>
+                    <th>Status</th>
+                    <th>Total</th>
+                    <th colspan="4"></th>
                 </tr>
             </thead>
             <tbody>
-                @foreach ($itens as $item)
+                @foreach ($pedidos as $pedido)
                     <tr>
-                        <td> <img width="60" src="{{ $item['foto'] }}" alt="" /></td>
-                        <td>{{ $item['descricao'] }}</td>
-                        <td>
-                            <div class="input-append">
-                                <input class="span1" style="max-width:34px;" id="appendedInputButtons" size="16"
-                                    type="text" placeholder="{{ $item['quantidade'] }}">
-                                <a href="{{ route('carrinho.diminuir', $item['produto_id']) }}">
-                                    <button class="btn" type="button">
-                                        <i class="icon-minus"></i>
-                                    </button>
-                                </a>
-                                <a href="{{ route('carrinho.adicionar', $item['produto_id']) }}">
-                                    <button class="btn" type="button">
-                                        <i class="icon-plus"></i>
-                                    </button>
-                                </a>
-                                <a href="{{ route('carrinho.remover', $item['produto_id']) }}">
-                                    <button class="btn btn-danger" type="button">
-                                        <i class="icon-remove icon-white"></i>
-                                    </button>
-                                </a>
-                            </div>
-                        </td>
-                        <td>{{ $item['preco'] }}</td>
-                        <td>{{ $item['desconto'] }}</td>
-                        <td colspan="2">{{ $item['total'] }}</td>
+                        <td> {{ $pedido->id}}</td>
+                        <td>{{ $pedido->status }}</td>
+                        <td>{{ $pedido->total }}</td>
+                        <td colspan="4"><a href="{{ route('pedido.cliente.visualizar', $pedido->id) }}" class="btn"> Visualizar</a></td>
                     </tr>
                 @endforeach
                 {{-- <tr>
@@ -129,22 +105,11 @@
                     @endforeach
                     <td> {{$total}}</td>
                 </tr> --}}
-                <tr>
-                    <td colspan="6" style="text-align:right">Desconto total: </td>
-                    
-                    <td> {{$totalDesconto}}</td>
-                </tr>
-                <tr>
-                    <td colspan="6" style="text-align:right"><strong>Total </strong></td>
-                    <td class="label label-important" style="display:block"> <strong> R${{$totalPreco}} </strong>
-                    </td>
-                </tr>
             </tbody>
         </table>
 
         <a href="{{route('home')}}" class="btn btn-large"><i class="icon-arrow-left"></i> Continue comprando
         </a>
-        <a href="{{route('carrinho.concluir',$pedido->id)}}" class="btn btn-large pull-right">Concluir <i class="icon-arrow-right"></i></a>
 
     </div>
 @endsection
